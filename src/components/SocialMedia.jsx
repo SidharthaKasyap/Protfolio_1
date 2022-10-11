aeccbcfc5de404935cd827d3cfebf2b50d014735
Sidhartha Kasyap <kasyapsidhartha@gmail.com>
2022-10-11 16:26:28 +0530

    fist commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <BsLinkedin />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
+export default SocialMedia;
